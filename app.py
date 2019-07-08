00ce230167b3e1115045f0960b223d1419d8923b
Vũ Đào Anh Tuấn <vudaoanhtuan@gmail.com>
2019-07-08 21:42:45 +0700

    update app, add predict current word

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route('/next_char', methods=['POST'])
 def get_next_char():
-    # return str(type(request.data))
     data = request.json
 
     if 'input' in data:
@@ -17,13 +16,25 @@ def get_next_char():
     else:
         return "error"
 
+@app.route('/current_word', methods=['POST'])
+def get_current_word():
+    data = request.json
+
+    if 'input' in data:
+        res = predictor.predict_current_word(data['input'])
+        res = json.dumps(res, ensure_ascii=False)
+        return res
+    else:
+        return "error"
+
+
 @app.route('/')
 def home():
     return send_file('index.html')
 
 if __name__ == '__main__':
     predictor = Predictor()
-    predictor.init_model('param/model.h5', 'param/vocab.h5')
+    predictor.init_model('weight/model.h5', 'weight/vocab.h5')
     port = int(os.environ.get('PORT', 5000))
-    host = '0.0.0.0'
+    host = 'localhost'
     app.run(host=host, port=port)
